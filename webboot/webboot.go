8808819674bd28ba1aba6bfee35d2e3c75a51f6e
Louis Murerwa <louis.b.murerwa.21@dartmouth.edu>
2019-07-22 11:14:54 -0700

    Added the weboot.go file where the webboot code will go.
    Added the webboot command to u-root so the command appears in u-root.
    
    Signed-off-by: Louis Murerwa <louis.b.murerwa.21@dartmouth.edu>

---
+++ webboot/webboot.go
@@ -0,0 +1,16 @@
+// Copyright 2013-2019 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Get the time the machine has been up
+// Synopsis:
+//     webboot
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("weboot up")
+}
