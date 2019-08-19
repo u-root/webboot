b33ebbc2bd8ce87beb81792a96e99d21451be82e
Louis Murerwa <louis.b.murerwa.21@dartmouth.edu>
2019-08-19 15:07:06 -0700

    WIP: Added the mountISOPmem function to mount /dev/pmem0 onto a directory.
    Signed-off-by: Louis Murerwa <louis.b.murerwa.21@dartmouth.edu>

--- pkg/mountkexec/mountkexec.go
+++ pkg/mountkexec/mountkexec.go
@@ -31,10 +31,19 @@ func MountISO(isoPath, mountDir string) error {
 	if err := loop.SetFile(loopDevice, isoPath); err != nil {
 		return fmt.Errorf("error setting loop device:%v", err)
 	}
-	var flags uintptr
-	flags |= unix.MS_RDONLY
-	if err = mount.Mount(loopDevice, mountDir, "iso9660", "", flags); err != nil {
+	if err = mount.Mount(loopDevice, mountDir, "iso9660", "", unix.MS_RDONLY); err != nil {
 		return fmt.Errorf("error mounting ISO:%v", err)
+	}
+	return nil
+}
+
+// MountISOPmem creates a directory and mounts the pmem block device on the directory
+func MountISOPmem(pmem, mountDir string) error {
+	if err := os.MkdirAll(mountDir, 777); err != nil {
+		return fmt.Errorf("error making mount directory:%v", err)
+	}
+	if err := mount.Mount(pmem, mountDir, "iso9660", "", unix.MS_RDONLY); err != nil {
+		return fmt.Errorf("error mounting %v on %v: %v", pmem, mountDir, err)
 	}
 	return nil
 }
