b3daf96681c544934f4710ac0f7ddbfeb2f3aa5e
bjakobson <brandonjakobson@gmail.com>
2020-02-06 21:06:56 -0800

    delete unnecessary function
    Signed-off-by: bjakobson <brandonjakobson@gmail.com>

--- pkg/webboot/webboot.go
+++ pkg/webboot/webboot.go
@@ -4,29 +4,10 @@
 
 package webboot
 
-import (
-	"github.com/u-root/u-root/pkg/cmdline"
-)
-
 //Distro defines an operating system distribution
 type Distro struct {
 	Kernel       string
 	Initrd       string
 	Cmdline      string
 	DownloadLink string
-}
-
-//CommandLine processes the command line arguments of the distro
-func CommandLine(distroCmd, commandln string) (string, error) {
-	if commandln != "" {
-		distroCmd = commandln
-	}
-	if distroCmd == "reuse-cmdline" {
-		procCmdLine := cmdline.NewCmdLine()
-		if procCmdLine.Err != nil {
-			return "", procCmdLine.Err
-		}
-		distroCmd = procCmdLine.Raw
-	}
-	return distroCmd, nil
 }
