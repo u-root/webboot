20563215a2757622cec8d56d8dd94ffdec61feab
shiyux <xushiyu1996@gmail.com>
2020-07-10 16:45:21 -0700

    Add GetInput and test
    Signed-off-by: shiyux <xushiyu1996@gmail.com>

--- pkg/menu/menu.go
+++ pkg/menu/menu.go
@@ -1,20 +1,83 @@
 package menu
 
 import (
-    ui "github.com/gizak/termui/v3"
-    "github.com/gizak/termui/v3/widgets"
+	"os"
+
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
 )
 
 const menuWidth = 50
 
 // newParagraph will return a paragraph object with given initial text.
-func newParagraph(initText string, Border bool, location int, wid int, ht int) *widgets.Paragraph{
-    p := widgets.NewParagraph()
-    p.Text = initText
-    p.Border = false
-    p.SetRect(0, location, wid, location+ht)
-    p.TextStyle.Fg = ui.ColorWhite
-    ui.Render(p)
-    return p
+func newParagraph(initText string, border bool, location int, wid int, ht int) *widgets.Paragraph {
+	p := widgets.NewParagraph()
+	p.Text = initText
+	p.Border = border
+	p.SetRect(0, location, wid, location+ht)
+	p.TextStyle.Fg = ui.ColorWhite
+	ui.Render(p)
+	return p
 }
 
+// GetInput will present an input box to user and return the user's input.
+// GetInout will check validation of input using isValid function.
+func GetInput(introwords string, location int, wid int, ht int, isValid func(string) (bool, string)) (string, error) {
+	// intro paragraph is to tell user what need to be input here
+	newParagraph(introwords, false, location, len(introwords)+4, 3)
+	location += 2
+
+	// input paragraph is where user input
+	input := newParagraph("", true, location, wid, ht+2)
+	location += ht + 2
+
+	// warning paragraph is to warn user their input is not valid
+	warning := newParagraph("", false, location, wid, 3)
+	ui.Render(warning)
+
+	// keep tracking all input from user
+	uiEvents := ui.PollEvents()
+	for {
+		e := <-uiEvents
+		if e.Type != ui.KeyboardEvent {
+			continue
+		}
+        switch e.ID {
+		// if user input q or control-c, exit the program.
+		case "q", "<C-c>":
+			ui.Close()
+			os.Exit(1)
+        // If user hit enter means he did his choice.
+		// So check whether the input is valid or not by isValid function.
+		// If isValid is nil, just return the text anyway.
+		// If the input is not acceptable, show a warning.
+		case "<Enter>":
+            if isValid == nil {
+				return input.Text, nil
+			}
+			valid, warningWords := isValid(input.Text)
+			// if input is vilid, directly return the input itself.
+			// else we should clear the input box and show warnings.
+			if valid {
+				return input.Text, nil
+			}
+			input.Text = ""
+			warning.Text = warningWords
+			ui.Render(input)
+			ui.Render(warning)
+		//If userhit backspace, remove the last input character
+		case "<Backspace>":
+			input.Text = input.Text[:len(input.Text)-1]
+			ui.Render(input)
+		// as long as user do not hit enter or q or control-c, assuming that user is still inputing his choice
+		default:
+			// clear warning when user input
+			if warning.Text != "" {
+				warning.Text = ""
+				ui.Render(warning)
+			}
+			input.Text += e.ID
+			ui.Render(input)
+		}
+	}
+}
--- pkg/menu/menu_test.go
+++ pkg/menu/menu_test.go
@@ -1,15 +1,35 @@
 package menu
 
 import (
-    "testing"
-    ui "github.com/gizak/termui/v3"
+	ui "github.com/gizak/termui/v3"
+	"strconv"
+	"testing"
 )
 
-func Test_newParagraph(t *testing.T){
-    if err := ui.Init(); err != nil {
-        t.Log(err)
-    }
-    defer ui.Close()
-    p := newParagraph("test", false, 0, 50, 3)
-    t.Log(p.Text)
+func Test_newParagraph(t *testing.T) {
+	if err := ui.Init(); err != nil {
+		t.Log(err)
+	}
+	defer ui.Close()
+	p := newParagraph("newParagraph test", false, 0, 50, 3)
+	t.Log(p.Text)
+}
+
+func Test_GetInput(t *testing.T) {
+	if err := ui.Init(); err != nil {
+		t.Log(err)
+	}
+	defer ui.Close()
+	isValid := func(input string) (bool, string) {
+        c, err := strconv.Atoi(input)
+		if err != nil || c < 0 {
+			return false, "your input is not a valid entry number."
+		}
+		return true, ""
+	}
+	input, err := GetInput("GetInput test", 0, 50, 1, isValid)
+	t.Log(input)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
 }
