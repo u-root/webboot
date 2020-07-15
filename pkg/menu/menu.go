d7871e2f5257ee06dd186370c356e67da5df22bb
shiyux <xushiyu1996@gmail.com>
2020-07-15 16:04:34 -0700

    add NewInputWindow(WIP)
    Signed-off-by: shiyux <xushiyu1996@gmail.com>

--- pkg/menu/menu.go
+++ pkg/menu/menu.go
@@ -1,6 +1,7 @@
 package menu
 
 import (
+	"fmt"
 	"io"
 
 	ui "github.com/gizak/termui/v3"
@@ -9,7 +10,7 @@ import (
 
 const menuWidth = 50
 
-// newParagraph will return a paragraph object with given initial text.
+// return a paragraph object with given initial text.
 func newParagraph(initText string, border bool, location int, wid int, ht int) *widgets.Paragraph {
 	p := widgets.NewParagraph()
 	p.Text = initText
@@ -55,7 +56,7 @@ func processInput(input, warning *widgets.Paragraph, isValid func(string) (strin
 	}
 }
 
-// GetInput will present an input box to user and return the user's input.
+// present an input box to user and return the user's input.
 // GetInput will check validation of input using isValid function.
 func GetInput(introwords string, location int, wid int, ht int, isValid func(string) (string, bool)) (string, error) {
 	intro := newParagraph(introwords, false, location, len(introwords)+4, 3)
@@ -69,5 +70,31 @@ func GetInput(introwords string, location int, wid int, ht int, isValid func(str
 	ui.Render(warning)
 
 	uiEvents := ui.PollEvents()
+	return processInput(input, warning, isValid, uiEvents)
+}
+
+// create a new ui window and display an input box.
+func NewCustomInputWindow(introwords string, wid int, ht int, isValid func(string) (string, bool)) (string, error) {
+	uiEvents := ui.PollEvents()
+	return internalNewInputWindow(introwords, wid, ht, isValid, uiEvents)
+}
+
+func internalNewInputWindow(introwords string, wid int, ht int, isValid func(string) (string, bool), uiEvents <-chan ui.Event) (string, error) {
+	if err := ui.Init(); err != nil {
+		return "", fmt.Errorf("Failed to initialize termui: %v", err)
+	}
+	defer ui.Close()
+
+	location := 0
+	intro := newParagraph(introwords, false, location, len(introwords)+4, 3)
+	location += 2
+	input := newParagraph("", true, location, wid, ht+2)
+	location += ht + 2
+	warning := newParagraph("", false, location, wid, 3)
+
+	ui.Render(intro)
+	ui.Render(input)
+	ui.Render(warning)
+
 	return processInput(input, warning, isValid, uiEvents)
 }
