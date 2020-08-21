542d7393f69d253607363e8fef47884b91b732f8
Gabriel Biren <gbiren@google.com>
2020-08-21 12:21:24 -0700

    Added tests for menu and webboot to the Travis shell script.
    Signed-off-by: Gabriel Biren <gbiren@google.com>

--- travis.sh
+++ travis.sh
@@ -8,4 +8,6 @@ if [ ! -f "/tmp/initramfs.linux_amd64.cpio" ]; then
     exit 1
 fi
 
+(cd cmds/webboot && go test -v)
+(cd pkg/menu && go test -v)
 (cd pkg/bootiso && sudo -E env "PATH=$PATH" go test -v) # need sudo to mount the test iso
