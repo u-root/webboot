a8fdade870823e88cee613ee3915a6871fc9a50c
Paul Gherghetta <pgher7285@gmail.com>
2021-08-02 15:35:42 -0500

    Updated nightly.sh to build the kernel and fixed a permission denied error.
    Signed-off-by: Paul Gherghetta <pgher7285@gmail.com>

--- nightly.sh
+++ nightly.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 git clone --depth 1 -b v5.6.14 git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git linux
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/iwlwifi/linux-firmware.git
 cp config-5.6.14 linux/.config
