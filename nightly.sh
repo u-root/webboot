6e4858bbde552c3a880faf51898e652f7fc4348e
Paul Gherghetta <pgher7285@gmail.com>
2021-08-02 10:29:27 -0500

    Updated nightly.sh to build the kernel.
    Signed-off-by: Paul Gherghetta <pgher7285@gmail.com>

--- nightly.sh
+++ nightly.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
-
+git clone --depth 1 -b v5.6.14 git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git linux
+git clone git://git.kernel.org/pub/scm/linux/kernel/git/iwlwifi/linux-firmware.git
+cp config-5.6.14 linux/.config
+(cd linux && make bzImage)
+GO111MODULE=off go run .
 go test ./integration
 
