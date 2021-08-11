653982a650d56834f0211e0d06b02f9c4814d8d9
Paul Gherghetta <pgher7285@gmail.com>
2021-08-11 15:21:16 -0500

    Split nightly.yml into two separate jobs to prevent the linux kernel building multiple times.
    Signed-off-by: Paul Gherghetta <pgher7285@gmail.com>

--- nightly.sh
+++ nightly.sh
@@ -3,5 +3,3 @@ git clone --depth 1 -b v5.6.14 git://git.kernel.org/pub/scm/linux/kernel/git/sta
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/iwlwifi/linux-firmware.git
 cp config-5.6.14 linux/.config
 (cd linux && make bzImage)
-GO111MODULE=off go run .
-go test ./integration
