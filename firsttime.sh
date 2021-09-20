a661335ad5853d93804090282df1497d3f7e7d5d
Ryan O'Leary <ryanoleary@google.com>
2021-09-19 20:22:29 -0700

    Fix CI build
    This likely will not fix booting on all distros, but I am hoping this
    brings us from 0 to 1.
    
    Changes:
    
    * Add missing dependencies such as kexec-tools.
    * Add missing UROOT_QEMU env var.
    * Disable fail-fast since most of our distros fail to boot at the
      moment.
    * Limit the number of jobs running in parallel with max-parallel=2.
    * Remove a checkout code action. I believe this is checking out code
      over the kernel which was just build. This would explain the "No files
      were found with the provided path: linux/arch/x86/boot/bzImage" error.
    * Remove these two builds: LHSCowboys and DHSGaels. This saves CPU
      minutes on our GitHub "free plan".
    
    Signed-off-by: Ryan O'Leary <ryanoleary@google.com>

--- firsttime.sh
+++ firsttime.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-sudo apt-get install wireless-tools wpasupplicant libnl-3-dev libnl-genl-3-dev
+sudo apt-get install build-essential kexec-tools libelf-dev libnl-3-dev libnl-genl-3-dev libssl-dev qemu-system-x86 wireless-tools wpasupplicant
 
 go get -u github.com/u-root/u-root
 go get -u github.com/u-root/NiChrome/...
