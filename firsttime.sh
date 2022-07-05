b7995f71fa15f3e9edfd5d754dc1e05355b8fe2e
Paul Zhang <paulz@ucla.edu>
2022-07-05 14:28:06 -0700

    Avoid installing NiChrome during setup
    Signed-off-by: Paul Zhang <paulz@ucla.edu>

--- firsttime.sh
+++ firsttime.sh
@@ -4,4 +4,3 @@ set -e
 sudo apt-get install build-essential kexec-tools libelf-dev libnl-3-dev libnl-genl-3-dev libssl-dev qemu-system-x86 wireless-tools wpasupplicant
 
 go get -u github.com/u-root/u-root
-go get -u github.com/u-root/NiChrome/...
